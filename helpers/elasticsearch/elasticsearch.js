a44c48c4cc842f34504c62eb282f84db59f08e91
Vojtěch Červený <cervik49@gmail.com>
2020-01-03 12:18:38 +0100

    simplify code

--- helpers/elasticsearch/elasticsearch.js
+++ helpers/elasticsearch/elasticsearch.js
@@ -24,54 +24,17 @@ function sendResultToElastic(data) {
 }
 
 const myReporter = {
-  jasmineStarted: function(suiteInfo) {
-    console.log("Running suite with " + suiteInfo.totalSpecsDefined);
-  },
-
-  suiteStarted: function(result) {
-    console.log(
-      "Suite started: " +
-        result.description +
-        " whose full description is: " +
-        result.fullName
-    );
-  },
-
-  specStarted: function(result) {
-    console.log(
-      "Spec started: " +
-        result.description +
-        " whose full description is: " +
-        result.fullName
-    );
-  },
-
-  specDone: function(result) {
-    console.log("Spec: " + result.description + " was " + result.status);
-
-    for (var i = 0; i < result.failedExpectations.length; i++) {
-      console.log("Failure: " + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
-    }
-
-    console.log(result.passedExpectations.length);
-  },
+  jasmineStarted: function(suiteInfo) {},
+  suiteStarted: function(result) {},
+  specStarted: function(result) {},
+  specDone: function(result) {},
 
   suiteDone: async function(result) {
-    console.log("Suite: " + result.description + " was " + result.status);
-    for (var i = 0; i < result.failedExpectations.length; i++) {
-      console.log("Suite " + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
-    }
     await sendResultToElastic(result);
   },
 
   jasmineDone: function(result) {
     console.log("Finished suite: " + result.overallStatus);
-    for (var i = 0; i < result.failedExpectations.length; i++) {
-      console.log("Global " + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
-    }
   }
 };
 
